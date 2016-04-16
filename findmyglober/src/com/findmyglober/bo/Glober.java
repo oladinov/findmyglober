d244e922e18577937aa5295f6a71a4269c14d764
Gerardo <gerardo.solorzano@globant.com>
2016-04-15 20:19:01 -0500

    Service changes

--- findmyglober/src/com/findmyglober/bo/Glober.java
+++ findmyglober/src/com/findmyglober/bo/Glober.java
@@ -1,32 +1,40 @@
 package com.findmyglober.bo;
 
 public class Glober {
-	
+
 	private String name;
 	private String email;
 	private String project;
 	private String site;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getProject() {
 		return project;
 	}
+
 	public void setProject(String project) {
 		this.project = project;
 	}
+
 	public String getSite() {
 		return site;
 	}
+
 	public void setSite(String site) {
 		this.site = site;
 	}
--- findmyglober/src/com/findmyglober/service/GloberService.java
+++ findmyglober/src/com/findmyglober/service/GloberService.java
@@ -9,12 +9,27 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.findmyglober.bo.Glober;
-
+/**
+ *
+ * Service calls
+ * 
+ * getByName
+ * /findmyglober/glober/getByName/{name}
+ * 
+ * getByEmail
+ * /findmyglober/glober/getByEmail/{email}
+ * 
+ * getByProject
+ * /findmyglober/glober/getByProject/{project}
+ * 
+ * @author Gerardo Solorzano
+ *
+ */
 @Path("/glober")
 public class GloberService {
 
@@ -23,26 +38,73 @@ public class GloberService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getByName(@PathParam("name") String globerName) throws JSONException {
 
-		List<JSONObject> listGlobers = new ArrayList<JSONObject>();
+		List<JSONObject> listResponse = new ArrayList<JSONObject>();
+		
 		JSONObject jsonObject1 = new JSONObject();
-
 		jsonObject1.put("name", globerName);
-		jsonObject1.put("email", "emailDummy");
-		jsonObject1.put("project", "projectDummy");
-		jsonObject1.put("site", "siteDummy");
+		jsonObject1.put("email", "emailDummy1");
+		jsonObject1.put("project", "projectDummy1");
+		jsonObject1.put("site", "siteDummy1");
 
 		JSONObject jsonObject2 = new JSONObject();
-
 		jsonObject2.put("name", globerName);
-		jsonObject2.put("email", "emailDummy");
-		jsonObject2.put("project", "projectDummy");
-		jsonObject2.put("site", "siteDummy");
+		jsonObject2.put("email", "emailDummy2");
+		jsonObject2.put("project", "projectDummy2");
+		jsonObject2.put("site", "siteDummy2");
 
-		listGlobers.add(jsonObject1);
-		listGlobers.add(jsonObject2);
+		listResponse.add(jsonObject1);
+		listResponse.add(jsonObject2);
 
-		String result = "" + listGlobers;
-		return Response.status(200).entity(result).build();
+		return Response.status(Status.OK).entity(listResponse.toString()).build();
 	}
+	
+	@Path("/getByEmail/{email}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getByEmail(@PathParam("email") String globerEmail) throws JSONException {
 
+		List<JSONObject> listResponse = new ArrayList<JSONObject>();
+		
+		JSONObject jsonObject1 = new JSONObject();
+		jsonObject1.put("name", "nameDummy1");
+		jsonObject1.put("email", globerEmail);
+		jsonObject1.put("project", "projectDummy1");
+		jsonObject1.put("site", "siteDummy1");
+
+		JSONObject jsonObject2 = new JSONObject();
+		jsonObject2.put("name", "nameDummy2");
+		jsonObject2.put("email", globerEmail);
+		jsonObject2.put("project", "projectDummy2");
+		jsonObject2.put("site", "siteDummy2");
+
+		listResponse.add(jsonObject1);
+		listResponse.add(jsonObject2);
+
+		return Response.status(Status.OK).entity(listResponse.toString()).build();
+	}
+	
+	@Path("/getByProject/{project}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getByProject(@PathParam("project") String project) throws JSONException {
+
+		List<JSONObject> listResponse = new ArrayList<JSONObject>();
+		
+		JSONObject jsonObject1 = new JSONObject();
+		jsonObject1.put("name", "nameDummy1");
+		jsonObject1.put("email", "emailDummy1");
+		jsonObject1.put("project", project);
+		jsonObject1.put("site", "siteDummy1");
+
+		JSONObject jsonObject2 = new JSONObject();
+		jsonObject2.put("name", "nameDummy2");
+		jsonObject2.put("email", "emailDummy2");
+		jsonObject2.put("project", project);
+		jsonObject2.put("site", "siteDummy2");
+
+		listResponse.add(jsonObject1);
+		listResponse.add(jsonObject2);
+
+		return Response.status(Status.OK).entity(listResponse.toString()).build();
+	}
 }
